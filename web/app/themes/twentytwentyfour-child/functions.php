b8da7c9ff9fe1c8d096bf333b7de5e5ea86ebf26
Sabi K <sabiku85@gmail.com>
2024-01-28 13:15:57 +0100

    Dev (#2)
    * T-4 Stworzenie repozytorium projektu - instalacja child theme oraz pluginów ACF, WP-Optimize, Updraft Plus
    
    * Podpięcie testoweg pliku css

--- web/app/themes/twentytwentyfour-child/functions.php
+++ web/app/themes/twentytwentyfour-child/functions.php
@@ -7,11 +7,10 @@ function theme_slug_enqueue_styles() {
 		get_stylesheet_uri()
 	);
 
-
   wp_enqueue_style( 
-    'theme-slug-primary',
-    get_parent_theme_file_uri( 'assets/css/main.css' )
-  );
+		'theme-slug-main',
+		get_theme_file_uri( 'assets/css/main.css' )
+	);
 }
 
 ?>
